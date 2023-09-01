fb6cd2925e799a083caabde6a6c65d09a6c20da5
ciguaran2 <iguarancarlos@gmail.com>
2023-09-01 17:49:57 -0500

    updating dropdown
    Co-Authored-By: ciguaran2 <139932367+ciguaran2@users.noreply.github.com>

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://${windows.location.hostname}:3000/api',
+    baseURL: 'http://localhost:3000/api',
     withCredentials: true
 })
 
--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import cors from 'cors'
 const app = express();
 
 app.use(cors({
-    origin: 'http://${windows.location.hostname}:5173',
+    origin: 'http://localhost:5173',
     credentials: true
 }))
 app.use(morgan('dev'))
