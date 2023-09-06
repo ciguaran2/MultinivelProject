e8babbdca169c5dc39d0ef964fcfc9e2007538b8
ciguaran2 <iguarancarlos@gmail.com>
2023-09-06 11:34:25 -0500

    new

--- src/App.js
+++ src/App.js
@@ -10,10 +10,7 @@ import cors from 'cors'
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
