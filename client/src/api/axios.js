8965afe50a2aa07d8aea1a7e42b9a928fbd2c1a4
ciguaran2 <iguarancarlos@gmail.com>
2023-09-08 14:51:45 -0500

    merging in Replit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,9 +2,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    //baseURL: 'http://localhost:3000/api',
+    baseURL: 'http://localhost:3000/api',
     //baseURL: 'https://susu-wayuu.ciguaran2.repl.co/api',
-    baseURL: 'https://susu-wayuu-backend.uc.r.appspot.com/api',
+    //baseURL: 'https://susu-wayuu-backend.uc.r.appspot.com/api',
     withCredentials: true
 })
 
