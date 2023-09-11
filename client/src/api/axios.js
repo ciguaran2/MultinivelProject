2e7223a8fc312cf84290c251dea28d1863a170dd
ciguaran2 <iguarancarlos@gmail.com>
2023-09-11 12:26:34 -0500

    ddd

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    withCredentials:true,
     //baseURL: 'http://localhost:3000/api',
     //baseURL: 'https://susu-wayuu.ciguaran2.repl.co/api',
-    baseURL: 'https://susu-397516.rj.r.appspot.com/api'
+    baseURL: 'https://susu-397516.rj.r.appspot.com/api',
+    withCredentials:true
 })
 export default instance
