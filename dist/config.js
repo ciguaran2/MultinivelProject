126552b018419eb325472a6a8499be7e8917f988
ciguaran2 <iguarancarlos@gmail.com>
2023-09-18 11:39:47 -0500

    sub

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.TOKEN_SECRET = void 0;
-var TOKEN_SECRET = 'some secret key';
-exports.TOKEN_SECRET = TOKEN_SECRET;
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _App = _interopRequireDefault(require("./App.js"));
-var _db = require("./db");
-(0, _db.connectDB)();
-_App["default"].listen(3000);
-console.log('Server on port', 3000);
